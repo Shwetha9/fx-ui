2605e925fd59a1294a43abe47a382262528ea647
Shwetha9 <shwetha9@gmail.com>
2024-07-27 11:51:28 +1000

    Added prime NG for demo

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
+import { Router } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+import { AnimateModule } from 'primeng/animate';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  standalone: true,
+  imports: [
+    AnimateModule,
+    CommonModule,
+    MenubarModule,
+    BadgeModule,
+    AvatarModule,
+    InputTextModule,
+    RippleModule,
+    CardModule,
+    ButtonModule,
+    PanelModule,
+  ],
+  animations: [],
+})
+export class HomeComponent implements OnInit {
+  items: MenuItem[] | undefined;
+  private router = inject(Router);
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Router',
+        icon: 'pi pi-palette',
+        items: [
+          {
+            label: 'Installation',
+            route: '/installation',
+          },
+          {
+            label: 'Configuration',
+            route: '/configuration',
+          },
+        ],
+      },
+      {
+        label: 'Programmatic',
+        icon: 'pi pi-link',
+        command: () => {
+          this.router.navigate(['/installation']);
+        },
+      },
+      {
+        label: 'External',
+        icon: 'pi pi-home',
+        items: [
+          {
+            label: 'Angular',
+            url: 'https://angular.io/',
+          },
+          {
+            label: 'Vite.js',
+            url: 'https://vitejs.dev/',
+          },
+        ],
+      },
+    ];
+  }
+}
