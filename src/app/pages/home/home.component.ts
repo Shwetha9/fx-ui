e3794e3f395b701e8410cc8a5009c77389d2d1ad
Shwetha Anand <shwetha.anand@simprogroup.com>
2025-01-09 13:40:21 +1100

    Add the tasks API change

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,11 @@
-import { Component, DestroyRef, OnDestroy, OnInit, inject, signal } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  OnDestroy,
+  OnInit,
+  inject,
+  signal,
+} from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
 import { BadgeModule } from 'primeng/badge';
@@ -12,6 +19,7 @@ import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
 import { AnimateModule } from 'primeng/animate';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
 import { DataService } from 'src/app/services/api-service';
 import { Product } from '../../models/product.model';
 import { map, Observable, Subject, takeUntil } from 'rxjs';
@@ -42,6 +50,7 @@ export class HomeComponent implements OnInit {
   dataService = inject(DataService);
   products$: Observable<Product[]> | undefined;
   products$$ = signal<Product[]>([]);
+  products: Product[] = [];
   destroyRef = inject(DestroyRef);
 
   items: MenuItem[] = [
@@ -83,24 +92,38 @@ export class HomeComponent implements OnInit {
   ];
 
   ngOnInit() {
+    // 1. Returns an observable stream of products
     this.products$ = this.dataService.getData().pipe(
-      takeUntilDestroyed(this.destroyRef),
-      map((data) => data.products),
+      takeUntilDestroyed(this.destroyRef), // Automatically unsubscribes when the component is destroyed
+      map((data) => data.products), // Transforms the response to extract just the products array
     );
 
+    // 2. Subscribes to the observable directly and sets the products array as a signal
     this.dataService.getData().subscribe({
       next: (res) => {
-        this.products$$.set(res.products);
-        console.log(res.products);
+        this.products$$.set(res.products); // Updates the reactive signal with the new products data
+        this.products = res.products; // Updates a plain property with the new products data
+        console.log(res.products); // Logs the products array
+      },
+      error: (err) => {
+        console.log(err); // Logs any errors during the HTTP request
+      },
+      complete: () => {
+        console.log('complete'); // Logs when the observable completes
+      },
+    });
+    this.dataService.getTasks().subscribe({
+      next: (res) => {
+        console.log(res);
       },
       error: (err) => {
         console.log(err);
       },
       complete: () => {
         console.log('complete');
-      }
+      },
     });
   }
 
-
+  //https://au-devops-v21.myqa.simprosuite.com/api/mobile/v1.0/companies/0/tasks/?module=TM
 }
--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,14 @@ export class DataService {
 
   getData(): Observable<any> {
     return this.http.get(this.apiUrl);
+  }
+
+  getTasks(): Observable<any> {
+    const token = 'Bearer 4277a7b506edc180f82334206c782f5de3bf5838';
+    const headers = new HttpHeaders().set('Authorization', token);
+    return this.http.get(
+      'https://au-devops-v21.myqa.simprosuite.com/api/mobile/v1.0/companies/0/tasks/?module=TM',
+      { headers },
+    );
   }
 }
